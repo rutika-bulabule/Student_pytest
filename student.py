7c7a401efa1d170db55a19972798f6cefcf52067
rutika-bulabule <rutikabulabule@gmail.com>
2025-12-27 13:06:15 +0530

    Saving the file

--- student.py
+++ student.py
@@ -2,14 +2,13 @@ def student_details(usn, name, division, age):
     result = (
         f"student usn: {usn}\n"
         f"student name: {name}\n"
-        f"division: {division}\n"
+        f"student division: {division}\n"
         f"age: {age}"
     )
     return result
 
 
 if __name__ == "__main__":
-    
     usn = "01fe24bca320"
     name = "rutika"
     division = "E"
